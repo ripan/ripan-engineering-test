ec4a146be3a606ff8acfb0ddf06db05a0f65e7cb
Ripan <er.ripankumar@gmail.com>
2016-07-24 22:14:10 +0100

    Fix serializer

--- app/serializers/material_serializer.rb
+++ app/serializers/material_serializer.rb
@@ -1,4 +1,9 @@
 class MaterialSerializer < ActiveModel::Serializer
-  attributes :id, :title, :episode, :year, :som, :eom, :aspect_ratio, :material_type, :delivery_channel_group, :duration
+  #attributes :id, :title, :episode, :year, :som, :eom, :aspect_ratio, :material_type, :delivery_channel_group, :duration
+  attributes :title, :duration, :supplier
   has_one :supplier
+
+  def supplier
+  	object.supplier.name
+  end
 end
