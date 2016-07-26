758cd1b9604ca552c6166b891baec0dafcc30bf2
ripan <er.ripankumar@gmail.com>
2016-07-26 17:00:20 +0100

    Fix update attribute

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,7 +2,6 @@ class Material < ActiveRecord::Base
   audited on: [:update, :destroy]
   validates :title, presence: true, uniqueness: true
   belongs_to :supplier
-  accepts_nested_attributes_for :supplier
   
   def duration
     self.eom - self.som
