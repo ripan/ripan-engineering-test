b1c8f0f47b66a8c665cac21e5bb3599c24c810be
ripan <er.ripankumar@gmail.com>
2016-07-27 12:03:48 +0100

    Add tests

--- spec/models/material_spec.rb
+++ spec/models/material_spec.rb
@@ -2,23 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Material, :type => :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  #it { should validate_presence_of :title }
-  # it "has a valid factory" do
-  #   FactoryGirl.create(:material).should be_valid
-  # end
-  # it "is invalid without a title" do
-  #   FactoryGirl.create(:material, title: nil).should be_invalid
-  # end
-  # it "is invalid without a supplier" do
-  #   FactoryGirl.create(:material, supplier: nil).should be_invalid
-  #end
   context 'validations' do
     it { is_expected.to validate_presence_of(:title) }
-    # it { is_expected.to validate_presence_of(:supplier) }
-    # it { is_expected.to validate_presence_of(:title) }
-    # it { is_expected.to validate_presence_of(:episode) }
-    # it { is_expected.to validate_presence_of(:year) }
-    # it { is_expected.to validate_presence_of(:duration) }
-    # it { is_expected.to validate_presence_of(:aspect_ratio) }
   end
+
+  let(:valid_attributes) {
+    FactoryGirl.attributes_for(:material)
+  }
+
+  let(:invalid_attributes) {
+    FactoryGirl.attributes_for(:material, title:nil)
+  }
+
+  it "is valid with a title" do
+    material = FactoryGirl.build(:material, valid_attributes)
+    expect(material).to be_valid
+  end
+  it "is invalid without a title" do
+    material = FactoryGirl.build(:material, invalid_attributes)
+    expect{material.save!}.to raise_error
+  end
+
 end
--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  let(:valid_attributes) {
+    FactoryGirl.attributes_for(:supplier)
+  }
+
+  let(:invalid_attributes) {
+    FactoryGirl.attributes_for(:supplier, name:nil)
+  }
+
+  it "is valid with a name" do
+    supplier = FactoryGirl.build(:supplier, valid_attributes)
+    expect(supplier).to be_valid
+  end
+  it "is invalid without a name" do
+    supplier = FactoryGirl.build(:supplier, invalid_attributes)
+    expect{supplier.save!}.to raise_error
+  end
+
 end
